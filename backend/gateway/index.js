675e8c4eb097a3ae8f7e4fcb1626487ee07dd12a
Naman Jain <jainn0984@gmail.com>
2026-09-03 18:33:15 +0530

    Added gateway

---
+++ backend/gateway/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import proxy from "express-http-proxy";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/auth",proxy(process.env.AUTH_SERVICE));
+
+app.get("/", (req, res) => {
+    res.send("Gateway is running");
+});
+
+app.listen(port, () => {
+    console.log(`Gateway is running on port ${port}`);
+});
