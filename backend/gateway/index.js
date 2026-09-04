7c47367ecb1682009669422db0f78499378a6dee
Naman Jain <jainn0984@gmail.com>
2026-09-04 12:58:54 +0530

    Added cors in gw

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import proxy from "express-http-proxy";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -8,6 +10,11 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(cors({
+origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+app.use(cookieParser());
 app.use("/auth",proxy(process.env.AUTH_SERVICE));
 
 app.get("/", (req, res) => {
