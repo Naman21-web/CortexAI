9a25b6f0393c14bab243b4c57b6d5151bcb9a369
Naman Jain <jainn0984@gmail.com>
2026-09-04 14:36:18 +0530

    Added getCurrentUser in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import Home from './pages/Home';
+import { useEffect } from 'react';
+import getCurrentUser from './features/getCurrentUser';
 
 const App = () => {
-  console.log("App Loaded")
-  return <Home />
+  console.log("App Loaded");
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getCurrentUser();
+      console.log(user);
+    };
+    fetchUser();
+  }, []);
+  return (
+    <>
+      <Home />
+    </>
+  )
 }
 
 export default App
