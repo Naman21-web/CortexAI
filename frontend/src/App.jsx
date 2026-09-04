1b95817934d18266f291b6ce0358edb882f37b6c
Naman Jain <jainn0984@gmail.com>
2026-09-04 14:16:25 +0530

    Added Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import { signInWithPopup } from 'firebase/auth';
-import { auth, googleProvider } from './../utils/firebase.js';
-import api from '../utils/axios.js';
-
-
-const handleLogin = async (token) => {
-  try{
-    const {data} = await api.post("/auth/login",token);
-    console.log(data);
-  }
-  catch(error){
-    console.log(error);
-  }
-}
-
-const googleLogin = async () => {
-  const data = await signInWithPopup(auth, googleProvider);
-  const token = await data.user.getIdToken();
-  console.log(token);
-  await handleLogin(token);
-}
+import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <div className='w-full h-screen bg-black flex justify-center items-center'>
-      <button onClick={googleLogin}  className='w-50 h-24 bg-white'>
-        Continue with google
-      </button>
-    </div>
-  )
+  console.log("App Loaded")
+  return <Home />
 }
 
 export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, googleProvider } from './../../utils/firebase.js';
+import api from '../../utils/axios.js';
+import {FcGoogle} from "react-icons/fc"
+
+
+function Home() {
+
+    console.log("Home Loaded")
+
+    const handleLogin = async (token) => {
+        try{
+            const {data} = await api.post("/auth/login",{token});
+            console.log(data);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    const googleLogin = async () => {
+        const data = await signInWithPopup(auth, googleProvider);
+        const token = await data.user.getIdToken();
+        console.log(token);
+        await handleLogin(token);
+    }
+  return (
+    <div className='h-screen flex bg-[#odof14] text-white  overflow-hidden'>
+      <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur'>
+        <div className='w-[340px] bg-[#13151c] border border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5'>
+            <div>
+                <h2 className='text-[17px] font-semibold text-slate-100 tracking tight'>Welcome to CortexAI</h2>
+                <p className='text-[13px] text-slate-500'>Please login to continue using app</p>
+            </div>
+            <button className='w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-black/90 bg-white hover:bg-gray-200  transition-all duration-150 cursor-pointer' onClick={googleLogin}>
+                <FcGoogle size={15}/>
+                Continue with Google
+            </button>
+        </div>  
+      </div>
+    </div>
+  )
+}
+
+export default Home;
