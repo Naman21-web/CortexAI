24f4a8c84d3a51f956e91d0e2685cf88fcba8103
Naman Jain <jainn0984@gmail.com>
2026-09-04 12:58:01 +0530

    Added Auth route

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import routes from "./routes/auth.route.js"
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.json());
+app.use("/",routes)
 app.get("/", (req, res) => {
     res.send("Auth service is running");
 });
