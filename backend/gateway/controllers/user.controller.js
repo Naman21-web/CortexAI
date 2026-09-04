9241ff8e19fee38aaaee0a3803eb2e3ba72ecfcf
Naman Jain <jainn0984@gmail.com>
2026-09-04 14:35:23 +0530

    Added Route to get currentUser

---
+++ backend/gateway/controllers/user.controller.js
@@ -0,0 +1,13 @@
+export const getCurrentUser = async (req,res) => {
+    try{
+        const user = req.user;
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        return res.status(200).json({user});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
