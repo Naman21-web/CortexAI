131e064d0372b7372a3bb765dd0e27e568d60a26
Naman Jain <jainn0984@gmail.com>
2026-09-05 13:34:45 +0530

    Added chat service

---
+++ backend/services/chat/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Chat service is running");
+});
+
+app.listen(port, () => {
+    console.log(`Chat service is running on port ${port}`);
+    connectDb();
+});
