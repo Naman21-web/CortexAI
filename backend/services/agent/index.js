65626a8175c5b3dc4ec2f181a22cbcb071399a6e
Naman Jain <jainn0984@gmail.com>
2026-09-06 12:21:18 +0530

    Added Agent Routes

--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import router from "./routes/agent.route.js";
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
-
+app.use("/",router);
 app.get("/", (req, res) => {
     res.send("Agent service is running");
 });
