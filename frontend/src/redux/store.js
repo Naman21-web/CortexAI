cfcee89cf0089a698773c5dd8c1b9d1a681c702e
Naman Jain <jainn0984@gmail.com>
2026-09-07 17:32:27 +0530

    Added conv slice in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import coversationReducer from './conversationSlice.js'
 
 const store = configureStore({
     reducer:{
-        user:userReducer
-    }
+        user:userReducer,
+        conversation:coversationReducer
+    },
 })
 
 export default store;
