07e1f47edc76d3f3dbb5731c69cc762b88dd2cc2
Naman Jain <jainn0984@gmail.com>
2026-09-08 18:33:42 +0530

    Added getMessages slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import coversationReducer from './conversationSlice.js'
+import messageReducer from './messageSlice.js'
 
 const store = configureStore({
     reducer:{
         user:userReducer,
-        conversation:coversationReducer
+        conversation:coversationReducer,
+        message:messageReducer
     },
 })
 
