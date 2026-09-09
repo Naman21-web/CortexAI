e476e89e55921b34de62300d0bcbf7c42a1d990d
Naman Jain <jainn0984@gmail.com>
2026-09-09 21:44:55 +0530

    Updated dotenv import

--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectDb from "./config/db.js";
 import router from "./routes/agent.route.js";
-
-dotenv.config();
 
 const port = process.env.PORT || 8000;
 
