16616c7c486117be43a49c21ecfd4c6e5ec26486
Naman Jain <jainn0984@gmail.com>
2026-09-09 21:49:43 +0530

    Corrected syntax error

--- backend/services/agent/routes/agent.route.js
+++ backend/services/agent/routes/agent.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { agent } from '../controllers/agent.controller';
+import { agent } from '../controllers/agent.controller.js';
 
-const router = express.router();
+const router = express.Router();
 
 router.post("/chat",agent);
 
